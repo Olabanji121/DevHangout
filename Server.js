26a9a9f86da53ac07202f5f0f398eab90cffba23
Olabanji121 <olabanjiabimbola@gmail.com>
2020-01-26 11:40:44 +0100

    update

--- Server.js
+++ Server.js
@@ -20,16 +20,9 @@ app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/post', require('./routes/api/post'))
 
 // serve static assets in production
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-if(process.env.NODE_ENV === 'production'){
-    // ser static folder
-
-    app.use(express.static('client/build'));
-
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ))
-    })
-}
+app.get('/*', (req,res)=> {res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'))})
 
 const PORT = process.env.PORT || 5000;
 
